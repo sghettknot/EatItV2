d5cd8064fa934a31480cc11a8dd83d285f3b94a4
sghettknot <57679628+sghettknot@users.noreply.github.com>
2021-02-16 14:08:58 +0700

    Sg's first commit

---
+++ app/src/main/java/com/finalproject/androideatitv2client/Common/Common.java
@@ -0,0 +1,11 @@
+package com.finalproject.androideatitv2client.Common;
+
+import com.finalproject.androideatitv2client.Model.UserModel;
+
+public class Common {
+    public static final String USER_REFERENCES = "Users";
+    public static final String POPULAR_CATEGORY_REF = "MostPopular";
+    public static final String BEST_DEALS_REF = "BestDeals";
+    public static UserModel currentUser;
+}
+
