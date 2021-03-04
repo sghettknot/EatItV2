9234f472fe72b9a449140e15cdf0bf03f9eac08a
sghettknot <watcharavuth2000@gmail.com>
2021-03-04 15:20:41 +0700

    SEARCH BUTTON DONE

--- app/src/main/java/com/finalproject/androideatitv2client/Adapter/MyCategoryAdapter.java
+++ app/src/main/java/com/finalproject/androideatitv2client/Adapter/MyCategoryAdapter.java
@@ -19,6 +19,7 @@ import com.finalproject.androideatitv2client.R;
 
 import org.greenrobot.eventbus.EventBus;
 
+import java.util.Collection;
 import java.util.List;
 
 import butterknife.BindView;
@@ -58,6 +59,10 @@ public class MyCategoryAdapter extends RecyclerView.Adapter<MyCategoryAdapter.My
     @Override
     public int getItemCount() {
         return categoryModelList.size();
+    }
+
+    public List<CategoryModel> getListCategory() {
+        return categoryModelList;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
--- app/src/main/java/com/finalproject/androideatitv2client/ui/menu/MenuFragment.java
+++ app/src/main/java/com/finalproject/androideatitv2client/ui/menu/MenuFragment.java
@@ -1,13 +1,22 @@
 package com.finalproject.androideatitv2client.ui.menu;
 
 import android.app.AlertDialog;
+import android.app.SearchManager;
+import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
+import android.widget.EditText;
 import android.widget.GridLayout;
+import android.widget.ImageView;
+import androidx.appcompat.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,7 +31,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.finalproject.androideatitv2client.Adapter.MyCategoryAdapter;
 import com.finalproject.androideatitv2client.Common.Common;
 import com.finalproject.androideatitv2client.Common.SpacesItemDecoration;
+import com.finalproject.androideatitv2client.Model.CategoryModel;
 import com.finalproject.androideatitv2client.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -63,6 +76,9 @@ public class MenuFragment extends Fragment {
     }
 
     private void initViews() {
+
+        setHasOptionsMenu(true);
+
         dialog = new SpotsDialog.Builder().setContext(getContext()).setCancelable(false).build();
         dialog.show();
         layoutAnimationController = AnimationUtils.loadLayoutAnimation(getContext(), R.anim.layout_item_from_left);
@@ -85,5 +101,55 @@ public class MenuFragment extends Fragment {
 
 
 
+    }
+
+    // Search Button
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.search_menu, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+
+        SearchManager searchManager = (SearchManager)getActivity().getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView)menuItem.getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+
+        // Event
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                startSearch(s);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+        // Clear text when click to clear button on search view
+        ImageView closeButton = (ImageView)searchView.findViewById(R.id.search_close_btn);
+        closeButton.setOnClickListener(view -> {
+            EditText ed = (EditText)searchView.findViewById(R.id.search_src_text);
+            // Clear text
+            ed.setText("");
+            // Collapse the action view
+            searchView.setQuery("", false);
+            // Collapse the search widget
+            menuItem.collapseActionView();
+            // Restore result to original
+            menuViewModel.loadCategories();
+        });
+    }
+
+    private void startSearch(String s) {
+        List<CategoryModel> resultList = new ArrayList<>();
+        for (int i=0; i < adapter.getListCategory().size(); i++) {
+            CategoryModel categoryModel = adapter.getListCategory().get(i);
+            if (categoryModel.getName().toLowerCase().contains(s))
+                resultList.add(categoryModel);
+        }
+        menuViewModel.getCategoryListMutable().setValue(resultList);
     }
 }
